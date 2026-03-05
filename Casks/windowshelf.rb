c52d2ba0192c3247a12e12e3c24eab5db0f744e0
pulsedriver2 <pulsedriver2@gmail.com>
2026-03-05 15:02:50 +0100

    Update WindowShelf to v1.0.89

--- Casks/windowshelf.rb
+++ Casks/windowshelf.rb
@@ -1,6 +1,6 @@
 cask "windowshelf" do
-  version "1.0.88"
-  sha256 "aaab21eb1c2f4c1fd5bf84d18a6899c276aa13577f617be7b52c94b6ed8c45d5"
+  version "1.0.89"
+  sha256 "cbb3bba714f6593f2c89f351633d7f6912914b11e30babac6781b1c1c7b94a86"
 
   url "https://windowshelf.app/WindowShelf.dmg"
   name "WindowShelf"
